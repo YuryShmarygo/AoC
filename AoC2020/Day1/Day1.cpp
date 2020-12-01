b244aee981b2127a912ac2ffab892d8588a5ee68
Yury <yury.shmarygo@gmail.com>
2020-12-01 05:41:26 +0000

    2020 Day1

--- AoC2020/Day1/Day1.cpp
+++ AoC2020/Day1/Day1.cpp
@@ -7,26 +7,37 @@
 
 using namespace std;
 
+long FindTwo(const vector<long>& input)
+{
+	for (auto it = input.begin(); it != input.end(); ++it)
+		for (auto it2 = next(it); it2 != input.end(); ++it2)
+			if (*it + *it2 == 2020)
+				return *it * *it2;
+	return 0;
+}
+long FindThree(const vector<long>& input)
+{
+	for (auto it = input.begin(); it != input.end(); ++it)
+		for (auto it2 = next(it); it2 != input.end(); ++it2)
+			for (auto it3 = next(it2); it3 != input.end() && *it + *it2 < 2020; ++it3)
+				if (*it + *it2 + *it3 == 2020)
+					return *it * *it2 * *it3;
+	return 0;
+}
+void Test()
+{
+	vector<long> input = { 1721, 979, 366, 299, 675, 1456 };
+	cout << "Test1: " << FindTwo(input) << endl;
+	cout << "Test2: " << FindThree(input) << endl;
+}
 int main()
 {
+	Test();
 	ifstream is("Day1.txt");
 	istream_iterator<long> start(is), end;
 	vector<long> input(start, end);
 	cout << "Read " << input.size() << " numbers" << endl;
 
-	long sum = 0;
-	for_each(input.begin(), input.end(), [&](auto i) {sum += i / 3 - 2; });
-	cout << "Day1 Answer1: " << sum << endl;
-
-	sum = 0;
-	for_each(input.begin(), input.end(), [&](auto i)
-		{
-			long fuel = i / 3 - 2;
-			while (fuel > 0)
-			{
-				sum += fuel;
-				fuel = fuel / 3 - 2;
-			}
-		});
-	cout << "Day1 Answer2: " << sum << endl;
+	cout << "Day1 Answer1: " << FindTwo(input) << endl;
+	cout << "Day1 Answer2: " << FindThree(input) << endl;
 }
